d434419e6dd2f7a89ff3cb1049626cab4589e5e8
2akariae <zakariaelahrache2@gmail.com>
2026-06-20 00:04:19 +0100

    fix(detective): refactor routing to avoid react-router-dom dependency

--- src/games/detective/routes/DetectiveRoutes.jsx
+++ src/games/detective/routes/DetectiveRoutes.jsx
@@ -1,6 +1,8 @@
 // src/games/detective/routes/DetectiveRoutes.jsx
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useDetectiveStore } from '../store/detectiveStore.js';
+import { PHASES } from '../constants/detectiveConstants.js';
+
 import { DetectiveLobby } from '../components/lobby/DetectiveLobby.jsx';
 import { SetupPhase } from '../components/phases/SetupPhase.jsx';
 import { WitnessPhase } from '../components/phases/WitnessPhase.jsx';
@@ -9,15 +11,22 @@ import { AccusationPhase } from '../components/phases/AccusationPhase.jsx';
 import { ResolutionScreen } from '../components/resolution/ResolutionScreen.jsx';
 
 export default function DetectiveRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<DetectiveLobby />} />
-      <Route path="/lobby" element={<DetectiveLobby />} />
-      <Route path="/room/:roomId" element={
-        // This would normally render the active phase component based on store
-        <SetupPhase /> 
-      } />
-      <Route path="/room/:roomId/result" element={<ResolutionScreen />} />
-    </Routes>
-  );
+  const { currentPhase } = useDetectiveStore();
+
+  switch (currentPhase) {
+    case PHASES.LOBBY:
+      return <DetectiveLobby key="lobby" />;
+    case PHASES.SETUP:
+      return <SetupPhase key="setup" />;
+    case PHASES.WITNESS:
+      return <WitnessPhase key="witness" />;
+    case PHASES.DISCUSSION:
+      return <DiscussionPhase key="discussion" />;
+    case PHASES.ACCUSATION:
+      return <AccusationPhase key="accusation" />;
+    case PHASES.RESOLUTION:
+      return <ResolutionScreen key="resolution" />;
+    default:
+      return <DetectiveLobby key="lobby-fallback" />;
+  }
 }
